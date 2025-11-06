b00a794b43054c231d1e996fd95ef4822d8b07dc
Михаил Тихонов <mishkatix88@gmail.com>
2025-11-06 16:28:53 +0200

    Initial Commit

---
+++ CoinTrack/CoinTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackApp.swift
+//  CoinTrack
+//
+//  Created by Михайло Тихонов on 06.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
